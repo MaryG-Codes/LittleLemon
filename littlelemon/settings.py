20fb97c4c248ae390880d1ec3017f5387d674163
MaryG-Codes <gerdes.mary89@gmail.com>
2026-04-15 17:46:06 -0500

    another change

--- littlelemon/settings.py
+++ littlelemon/settings.py
@@ -76,6 +76,10 @@ WSGI_APPLICATION = 'littlelemon.wsgi.application'
 
 DATABASES = {
     'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    },
+    'mydb': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'reservations',
         'HOST' : '127.0.0.1',
@@ -83,6 +87,7 @@ DATABASES = {
         'USER' : 'root',
         'PASSWORD' : '',
     }
+
 }
 
 MEDIA_URL = '/media/'
