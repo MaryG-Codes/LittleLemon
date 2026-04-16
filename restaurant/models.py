f0836fc4d2342814ad34f102f2a22359104560f2
MaryG-Codes <gerdes.mary89@gmail.com>
2026-04-16 09:50:16 -0500

    changed restaurant models

--- restaurant/models.py
+++ restaurant/models.py
@@ -5,6 +5,7 @@ from django.db import models
 class Booking(models.Model):
     first_name = models.CharField(max_length=200)
     reservation_date = models.DateField()
+    No_of_guests = models.IntegerField(default=1)
     reservation_slot = models.SmallIntegerField(default=10)
 
     def __str__(self): 
@@ -15,6 +16,7 @@ class Menu(models.Model):
    name = models.CharField(max_length=200) 
    price = models.IntegerField(null=False) 
    menu_item_description = models.TextField(max_length=1000, default='') 
+   inventory = models.IntegerField(default=0)
 
    def __str__(self):
       return self.name
